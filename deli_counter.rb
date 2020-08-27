c4b5de3ffadd0ec8b1d4464eb0e8c697fe7efa93
Haidy  Farahat <haidyfarahat@gmail.com>
2020-08-27 04:34:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,8 +12,7 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, name)
-    katz_deli.push ("Ada"), ("Grace"), ("Kent")
-    puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+    katz_deli.push ("name")
 end
 
 def now_serving (katz_deli)
